0ed0c711dc20bd28804dcf0d857940b1533c0f12
Shagun Shaw <shagunshaw2006@gmail.com>
2026-05-09 21:48:03 +0530

    refactor: streamline authentication checks and improve navigation flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AppRoutes } from './routes/AppRoutes';
-import { useAuthStore } from './store/useAuthStore';
 
 function App() {
-  const { checkAuth } = useAuthStore();
-
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
-
   return (
     <BrowserRouter>
       <Toaster position="top-right" />
